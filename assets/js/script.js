26b2cbaee058e2bcba14325d65d26ef436c66679
Killa J <joshuaalexandercross@gmail.com>
2021-03-13 15:58:36 -0600

    added removal area to drag tasks into

--- assets/js/script.js
+++ assets/js/script.js
@@ -62,16 +62,12 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function(event) {
-    console.log("activate", this);
   },
   deactivate: function(event) {
-    console.log("deactivate", this);
   },
   over: function(event) {
-    console.log("over", event.target);
   },
   out: function(event) {
-    console.log("out", event.target);
   },
   update: function(event) {
     var tempArr = [];
@@ -102,6 +98,21 @@ $(".card .list-group").sortable({
     // update array on tasks object and save
     tasks[arrName] = tempArr;
     saveTasks();
+  }
+});
+
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    console.log("drop");
+      ui.draggable.remove();
+    },
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function(event, ui) {
+    console.log("out");
   }
 });
 
